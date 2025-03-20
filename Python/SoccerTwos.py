98b3b26f6e2c02ccdcfd8377b24f5b956eda8b00
togu6669 <togu6669@gmail.com>
2025-03-20 13:40:16 +0100

    SoccerTwos training for Mac

--- Python/SoccerTwos.py
+++ Python/SoccerTwos.py
@@ -7,13 +7,25 @@ pip install -e ./ml-agents-envs
 pip install -e ./ml-agents
 
 
+
+git-lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | os=debian dist=jessie sudo -E bash
 sudo apt-get install git-lfs
 
 
+Mac only:
+after downloading app you need to add exex attributes:
+xattr -cr training-envs-executables/SoccerTwos/SoccerTwos.app
+
+
+Linux version, there was a broken venv which not installed properly and I did not have a relative paths
 python3 ml-agents/mlagents/trainers/learn.py ./config/poca/SoccerTwos.yaml --env=./training-envs-executables/SoccerTwos/SoccerTwos.x86_64 --run-id="SoccerTwos" --no-graphics
 
 
-huggingface-cli login
+Mac version of learning with correct venv:
+mlagents-learn ./config/poca/SoccerTwos.yaml --env=./training-envs-executables/SoccerTwos/SoccerTwos.app --run-id="SoccerTwos" --no-graphics
 
+
+huggingface-cli login
 mlagents-push-to-hf  --run-id="SoccerTwos" --local-dir="./results/SoccerTwos" --repo-id="togu6669/poca-SoccerTwos" --commit-message="First Push"`
+
